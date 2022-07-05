19082542ca11ee70a3ed64687b4f5879738d0975
spinales <saul021002@gmail.com>
2022-07-05 16:19:13 -0400

    variables de entorno

---
+++ util/config.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBURI               string
+	AppVersion          string
+	DBVersion           uint
+	ServerAddress       string
+	TokenSymmetricKey   string
+	AccessTokenDuration time.Duration
+}
+
+func LoadConfig() Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	accessTokenDuration := os.Getenv("Access_Token_Duration")
+	num, err := strconv.Atoi(accessTokenDuration)
+	if err != nil {
+		log.Panicln("Invalid time unit in .env file, Access Token Duration")
+		return Config{}
+	}
+
+	return Config{
+		DBURI:               os.Getenv("DATABASE_URL"),
+		AppVersion:          "v0.1",
+		DBVersion:           1, // current db version
+		ServerAddress:       os.Getenv("Server_Address"),
+		TokenSymmetricKey:   "", // pending
+		AccessTokenDuration: time.Hour * time.Duration(num),
+	}
+}
