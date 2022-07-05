be98b13b0fc54ee94cb7c2fa9761c2e60d6b75d2
spinales <saul021002@gmail.com>
2022-07-05 16:20:56 -0400

    metodos de base de datos de user

---
+++ storage/cockroach/user.go
@@ -0,0 +1,46 @@
+package cockroach
+
+import (
+	"github.com/spinales/ASIA-backend/models"
+	"gorm.io/gorm"
+)
+
+type UserService struct {
+	DB *gorm.DB
+}
+
+// UserByTuition search the user in db by tuition
+func (s *UserService) UserByTuition(tuition string) (*models.User, error) {
+	var user models.User
+	s.DB.Where("tuition=?", tuition).First(&user)
+	return &user, nil
+}
+
+func (s *UserService) AddUser(user *models.User) (*models.User, error) {
+	s.DB.Create(user)
+	return user, nil
+}
+
+func (s *UserService) User(id uint) (*models.User, error) {
+	var cou models.User
+	s.DB.First(&cou, id)
+	return &cou, nil
+}
+
+func (s *UserService) UserByFirstname(name string) (*[]models.User, error) {
+	var users []models.User
+	s.DB.Where(&models.User{Firstname: name}).Find(&users)
+	return &users, nil
+}
+
+func (s *UserService) Deleteuser(id uint) error {
+	s.DB.Delete(&models.User{}, id)
+	return nil
+}
+
+func (s *UserService) UpdateUser(user *models.User, id uint) (*models.User, error) {
+	var c models.User
+	c.ID = id
+	s.DB.Model(&c).Updates(user)
+	return user, nil
+}
